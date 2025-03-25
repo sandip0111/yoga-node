d84c03c805a92f50fbced600fe6a6011d9193e9a
sandip <dassandip@190gmail.com>
2025-03-25 16:01:13 +0530

    test key update

--- controller/adminController.js
+++ controller/adminController.js
@@ -33,7 +33,7 @@ const timeSlots = require('../models/TimeSlots');
 const liveCoursesCustomermodel = require('../models/liveCoursesCustomerModel');
 const stripe = require('stripe')('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');
 const jwt = require('jsonwebtoken');
-// const stripe = require('stripe')('sk_test_51LTjKYSDnZBoIVm7EqcwQWYWStbdS5gufmBl3BLFKAReBHCSJharwcKHURUMEv3gq4oFCuDnDAO59WnKmpMPqi0100n0lg3QtG');
+// const stripe = require('stripe')('sk_test_51LJJXISEQq0H4GuE57DEzlM4vmKExUoPzoTFZzc6CclsIMQw8bJAzrnVJyxagwuUxwsAb1qCeoE0tp540gK9GiXO00E23soewI');
 
 // const nodeCCAvenue = require('node-ccavenue');
 // const ccav = new nodeCCAvenue.Configure({
