0050d837397d3b478dc99f3ea27ecfe0b52b314c
Sandip Das <dassandip190@gmail.com>
2025-11-18 21:26:39 +0530

    Default date december

--- models/liveCoursesCustomerModel.js
+++ models/liveCoursesCustomerModel.js
@@ -42,7 +42,7 @@ const liveCoursesCustomerSchema = new mongoose.Schema({
   },
   month: {
     type: String,
-    default: () => "November",
+    default: () => "December",
   },
   paymentType: {
     type: String,
