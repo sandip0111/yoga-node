346137022ccb9d3b857b547824f2726220317151
kingshukmath2014 <kingshukmath2014@gmail.com>
2025-11-26 23:46:20 +0530

    feat: Implement payment processing service with Razorpay and Stripe, including coupon generation, student course updates, and confirmation notifications via email and WhatsApp.

--- services/paymentService.js
+++ services/paymentService.js
@@ -756,6 +756,7 @@ function getRazorPaymentResultRishikesh(reqBody) {
             replacements: {
               NAME: user.name,
               WLINK: constants.LINK["100_HOURS_RISHIKESH"],
+              BOOK_LINK: constants.LINK.RISHIKESH_BOOKS,
             },
             mailTo: user.email,
             contentPath: fileName,
