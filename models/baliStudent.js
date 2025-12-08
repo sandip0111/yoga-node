966e3579828085197405d109a135b77675164ee5
kingshukmath2014 <kingshukmath2014@gmail.com>
2025-12-08 23:47:13 +0530

    feat: add Mongoose model for Bali student data

---
+++ models/baliStudent.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+
+const baliStudentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  hour: {
+    type: Number,
+  },
+  room: {
+    type: String,
+  },
+  currency: {
+    type: String,
+  },
+  price: {
+    type: String,
+  },
+  created: {
+    type: Date,
+    default: function () {
+      return new Date();
+    },
+  },
+  paymentStatus: {
+    type: String,
+    enum: ["pending", "paid", "failed"],
+    required: false,
+    default: "pending",
+  },
+  paymentId: {
+    type: String,
+    required: false,
+  },
+  paymentType: {
+    type: String,
+    enum: ["stripe", "paypal"],
+    required: false,
+    default: null,
+  },
+  orderId: {
+    type: String,
+    required: false,
+  },
+  isPaymentCheck: {
+    type: Boolean,
+    default: false,
+  },
+  month: {
+    type: String,
+    default: () => "March, 2026",
+  },
+});
+
+module.exports = mongoose.model(
+  "baliStudentModel",
+  baliStudentSchema
+);
