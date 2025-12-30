d0fa85076d13ae831cc35e3f8aed73e48ce2cb86
Kaushik <kaushik.das.mca18@gmail.com>
2025-12-30 14:00:33 +0530

    month update

--- models/liveCoursesCustomerModel.js
+++ models/liveCoursesCustomerModel.js
@@ -42,7 +42,7 @@ const liveCoursesCustomerSchema = new mongoose.Schema({
   },
   month: {
     type: String,
-    default: () => "January",
+    default: () => "January, 2026",
   },
   paymentType: {
     type: String,
