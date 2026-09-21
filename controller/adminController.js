98efe9d83820d0714ef475970054e77ccf420daf
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-09-21 10:43:29 +0530

    Update free webinar email template and change default email subject

--- controller/adminController.js
+++ controller/adminController.js
@@ -2894,7 +2894,7 @@ module.exports = {
     try {
       const emailSubject =
         req.body.emailSubject?.trim() ||
-        "[REMINDER] “Into the roots of Yoga”";
+        "Today! Story of yoga 🪷";
       const limit = parseInt(req.body.limit) || 500;
 
       const courseRepo = require("../repositories/courseRepository");
