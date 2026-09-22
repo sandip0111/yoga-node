fd014fe66378114b93fb57b5be1a7b4c7512beab
kingshukmath2014 <kingshukmath2014@gmail.com>
2026-09-22 17:53:30 +0530

    Update free webinar email template and change default email subject to "Today! Pranayama as it is. 🌬️"

--- controller/adminController.js
+++ controller/adminController.js
@@ -2894,7 +2894,7 @@ module.exports = {
     try {
       const emailSubject =
         req.body.emailSubject?.trim() ||
-        "We are live! Traditional Hatha Yoga Class.";
+        "Today! Pranayama as it is. 🌬️";
       const limit = parseInt(req.body.limit) || 500;
 
       const courseRepo = require("../repositories/courseRepository");
